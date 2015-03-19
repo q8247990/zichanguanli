fa843361de64d99e4d4f98d925a449cf723bc3a2
q8247990 <q8247990@gmail.com>
2015-03-19 10:57:42 +0800

    123

---
+++ src/net/sourceforge/zbar/android/CameraTest/Maintest.java
@@ -0,0 +1,40 @@
+package net.sourceforge.zbar.android.CameraTest;
+import net.sourceforge.zbar.android.CameraTest.CameraPreview;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.Button;
+
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
+
+import android.widget.TextView;
+import android.graphics.ImageFormat;
+
+/* Import ZBar Class files */
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+import net.sourceforge.zbar.Config;
+
+public class Maintest extends Activity {
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        
+	        }
+	 
+	 
+
+}
